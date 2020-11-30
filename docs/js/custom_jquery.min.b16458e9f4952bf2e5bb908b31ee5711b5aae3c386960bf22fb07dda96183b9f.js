74292544fdd408e39eba4861136f392059875795
Christopher Campbell <chjacamp@gmail.com>
2020-11-30 16:43:23 -0600

    javascript has some problems. like, in R, I would never expect a free floating variable to be assigned a value (except for maybe constants). this whole 'document' thing has me shook.

---
+++ docs/js/custom_jquery.min.b16458e9f4952bf2e5bb908b31ee5711b5aae3c386960bf22fb07dda96183b9f.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.post-title').each(function(){var upper=$(this).html();$(this).replaceWith(upper.replace(/(\w)(\w*)/g,(_,firstChar,rest)=>firstChar+rest.toLowerCase()));});});
--- themes/anatole/assets/js/custom_jquery.js
+++ themes/anatole/assets/js/custom_jquery.js
@@ -1,4 +1,4 @@
- $('document').ready(function() {
+$(document).ready(function() {
    
   $('.post-title').each(function () {
     var upper = $(this).html();
